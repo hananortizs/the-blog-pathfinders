5c5f437eab76fb2cdbaea545ee256cdb58d97c5c
hananortizs <hanan..ortiz.hnnrtz@gmail.com>
2025-07-10 16:18:37 -0300

    add criei as primeiras paginas

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from 'next';
 import './globals.css';
-import clsx from 'clsx';
 
 export const metadata: Metadata = {
   title: 'The Blog - Este é um blog com next.js',
@@ -14,17 +13,7 @@ export default function RootLayout({ children }: Readonly<RootLayoutProps>) {
   return (
     <html lang='pt-BR'>
       <body>
-        <div
-          className={clsx(
-            'text-6xl',
-            'font-bold',
-            'hover:text-blue-50',
-            'hover:',
-            'bg-blue-500',
-          )}
-        >
-          {children}
-        </div>
+        <div>{children}</div>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,21 @@
-export default function HomePage() {
-  return <h1>Olá de dentro de page.tsx</h1>;
+import { PostsList } from '@/components/PostsList';
+import { SpinLoader } from '@/components/SpinLoader';
+import { Suspense } from 'react';
+
+export default async function HomePage() {
+  return (
+    <div className='text-slate-900 bg-slate-100 min-h-screen dark:text-slate-100 dark:bg-slate-900'>
+      <header>
+        <h1 className='text-6xl font-bold text-center py-8'>Aqui é a HEADER</h1>
+      </header>
+
+      <Suspense fallback={<SpinLoader />}>
+        <PostsList />
+      </Suspense>
+
+      <footer>
+        <h1 className='text-6xl font-bold text-center py-8'>Aqui é a FOOTER</h1>
+      </footer>
+    </div>
+  );
 }
